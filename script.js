d1ab95d88187d5650798f00fb877c87c0ef3c694
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 23:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function volume_sphere() {
     //Write your code here
-  
+
+    let radius = parseFloat(document.getElementById("radius").value);
+
+    let volume;
+
+    if (isNaN(radius) || radius < 0) {
+
+        volume = NaN;
+
+    } else {
+
+        volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+        volume = volume.toFixed(4);
+    }
+
+    document.getElementById("volume").value = volume;
+
+    return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
